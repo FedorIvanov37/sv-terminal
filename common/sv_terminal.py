a5c27a49efe7255e6464270ba2613e89440e240b
admin <fedorivanov37@gmail.com>
2023-08-06 11:03:34 +0300

    Small refactoring

--- common/sv_terminal.py
+++ common/sv_terminal.py
@@ -7,18 +7,20 @@
 #
 
 
+# Correct way to run
+if __name__ != "__main__":  # Runs only by import command
+    from common.gui.core.SvTerminalGui import SvTerminalGui
+    from common.gui.constants.TermFilesPath import TermFilesPath
+    from common.lib.data_models.Config import Config
+    from sys import exit
+
+    config: Config = Config.parse_file(TermFilesPath.CONFIG)
+    terminal: SvTerminalGui = SvTerminalGui(config)
+    status: int = terminal.run()
+    exit(status)
+
+
+# Incorrect way to run
 if __name__ == "__main__":  # Do not run directly
     raise RuntimeError(f"The file common/sv_terminal.py should be imported from main working directory, "
                        "direct run has no effect")
-
-
-from common.gui.core.SvTerminalGui import SvTerminalGui
-from common.gui.constants.TermFilesPath import TermFilesPath
-from common.lib.data_models.Config import Config
-from sys import exit
-
-
-config: Config = Config.parse_file(TermFilesPath.CONFIG)
-terminal: SvTerminalGui = SvTerminalGui(config)
-status: int = terminal.run()
-exit(status)
