1a8f1a329d3aefcd9475890ef99b6af64de362c0
admin <fedorivanov37@gmail.com>
2023-08-13 13:56:56 +0300

    JSON format backward compatibility

--- common/lib/core/JsonConverter.py
+++ common/lib/core/JsonConverter.py
@@ -3,13 +3,17 @@ from pydantic import ValidationError
 from common.lib.data_models.Transaction import Transaction, OldTransactionModel
 
 
-# Temporary solution for backward compatibility of JSON transaction messages
+"""
+Temporary solution for backward compatibility of JSON transaction messages. JSON files format was simplified in v0.15. 
+
+Guess the correct transaction model - old or new. In case of old style rewrites JSON file by new model data 
+"""
 
 
 class JsonConverter:
 
     @staticmethod
-    def convert(filename):
+    def convert(filename):  # Check data type and rewrite JSON-file when need
         if JsonConverter.get_transaction_model(filename) is OldTransactionModel:
             JsonConverter.convert_json(filename)
 
@@ -31,15 +35,22 @@ class JsonConverter:
         return transaction
 
     @staticmethod
-    def get_transaction_model(filename):  # Temporary solution for transfer period
-        try:
+    def get_transaction_model(filename):  # Gues the file format
+
+        try:  # Try to parse in old style
             OldTransactionModel.parse_file(filename)
             return OldTransactionModel
 
-        except ValidationError as validation_error:
+        except ValidationError as validation_error:  # Check the result in case of fail
+
+            # Such reject usually happens when we try to parse new data by old model
             err = validation_error.errors()[0]
 
-            if err["loc"][0] == 'config' and err["msg"] == 'field required':
-                return Transaction
+            try:
+                if err["loc"][0] == 'config' and err["msg"] == 'field required':
+                    return Transaction
+
+            except KeyError:
+                raise validation_error
 
             raise validation_error
