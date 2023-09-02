dfe6d85de5c7681e503814d2207e4a2695c9b873
admin <fedorivanov37@gmail.com>
2023-09-03 00:18:17 +0300

    Refactoring of JSON view

--- common/gui/windows/spec_unsaved.py
+++ common/gui/windows/spec_unsaved.py
@@ -4,6 +4,7 @@ from PyQt6.QtGui import QCloseEvent, QKeyEvent, QPixmap
 from PyQt6.QtCore import Qt, pyqtSignal
 from common.gui.decorators.window_settings import set_window_icon, has_close_button_only
 from common.gui.constants.GuiFilesPath import GuiFilesPath
+from common.gui.constants.ButtonActions import ButtonAction
 
 
 class SpecUnsaved(Ui_SpecUnsaved, QDialog):
@@ -27,18 +28,14 @@ class SpecUnsaved(Ui_SpecUnsaved, QDialog):
     @has_close_button_only
     def setup(self):
         self.LogoLabel.setPixmap(QPixmap(GuiFilesPath.MAIN_LOGO))
-
-        apply_menu = QMenu()
-
-        for action in ("For current session", "Permanently"):
-            apply_menu.addAction(action, self.need_apply)
-
-        self.ButtonSave.setMenu(apply_menu)
+        self.ButtonSave.setMenu(QMenu())
+        self.ButtonSave.menu().addAction(ButtonAction.ONE_SESSION, lambda: self.need_apply(ButtonAction.ONE_SESSION))
+        self.ButtonSave.menu().addSeparator()
+        self.ButtonSave.menu().addAction(ButtonAction.PERMANENTLY, lambda: self.need_apply(ButtonAction.PERMANENTLY))
         self.ButtonReturn.clicked.connect(self.return_to_spec.emit)
 
-    def need_apply(self):
-        commit = self.sender().text().upper() == "PERMANENTLY"  # TODO Hardcode
-        self.save.emit(commit)
+    def need_apply(self, commit: str):
+        self.save.emit(commit == ButtonAction.PERMANENTLY)
         self.accept()
 
     def closeEvent(self, a0: QCloseEvent) -> None:
