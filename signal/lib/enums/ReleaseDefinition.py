b39e9ec7f63bd13e6b54271724634e4dfc6ef882
admin <fedorivanov37@gmail.com>
2024-04-06 14:57:27 +0300

    Specified release version

--- signal/lib/enums/ReleaseDefinition.py
+++ signal/lib/enums/ReleaseDefinition.py
@@ -5,7 +5,7 @@ class ReleaseDefinition(StrEnum):
     EMAIL = "fedornivanov@gmail.com"
     AUTHOR = "Fedor Ivanov"
     VERSION = "v0.18"
-    RELEASE = "Apr 2023"
+    RELEASE = "Apr 2024"
     CONTACT = (f"<a href=\"mailto:{EMAIL}?subject=SIGNAL's user request&body=Dear Fedor,\n\n\n"
                            f"> Put your request here < \n\n\n\n"
                            f"My SIGNAL version is {VERSION} | Released in {RELEASE}\">{EMAIL}</a>")
