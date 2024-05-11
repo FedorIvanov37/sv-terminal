c3d68cff69817c259efafed3877bc713b2f7c35b
admin <fedorivanov37@gmail.com>
2024-05-12 02:50:45 +0300

    Builder

---
+++ build_scripts/clean_pycache.py
@@ -0,0 +1,24 @@
+from os import listdir, path, getcwd
+from shutil import rmtree
+
+
+REMOVE_DIR = "__pycache__"
+WORKDIR = getcwd()
+
+
+def clean(parent_dir):
+
+    for file in listdir(parent_dir):
+        file_path = path.join(parent_dir, file)
+
+        if not path.isdir(file_path):
+            continue
+
+        if not file == REMOVE_DIR:
+            clean(file_path)
+            continue
+
+        rmtree(file_path)
+
+
+clean(WORKDIR)
