83fb35e9a219f3ee9d7501fac511fa0097ecabe1
admin <fedorivanov37@gmail.com>
2024-05-11 22:02:36 +0300

    Builder

--- build_scripts/cleanup_release.py
+++ build_scripts/cleanup_release.py
@@ -1,9 +1,26 @@
-from os import chdir, listdir, unlink
+from os import listdir, path, remove
+from shutil import rmtree
+
 
 WORKDIR = "build"
+CLEANUP_DIRS = ["common/log", "common/data/spec_backup"]
+CLEANUP_FILES = ["common/data/license/license_info.json"]
 
-chdir(WORKDIR)
 
-for file in listdir('.'):
-    if file == "messages":
-        unlink(file)
+for directory in CLEANUP_DIRS:
+    for file in listdir(directory):
+        file_path = path.join(directory, file)
+
+        if path.isfile(file_path):
+            remove(file_path)
+
+        if path.isdir(file_path):
+            rmtree(file_path)
+
+
+for file_path in CLEANUP_FILES:
+
+    if not path.isfile(file_path):
+        continue
+
+    remove(file_path)
