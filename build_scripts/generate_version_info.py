8ee2ab6daac5da3851188020f9b3e8274086d5c9
admin <fedorivanov37@gmail.com>
2024-05-19 05:12:46 +0300

    Add version info

---
+++ build_scripts/generate_version_info.py
@@ -0,0 +1,38 @@
+from os import chdir
+
+
+WORKDIR = "build"
+OUTPUT_FILE = "version_info.rc"
+VERSION_INFO = r"""
+1 VERSIONINFO
+FILEVERSION 0,0,0,18
+PRODUCTVERSION 0,0,0,18
+FILEOS 0x40004
+FILETYPE 0x1
+{
+BLOCK "StringFileInfo"
+{
+	BLOCK "040904B0"
+	{
+		VALUE "FileDescription", "Simplified ISO generation algorithm"
+		VALUE "InternalName", "signal.exe"
+		VALUE "OriginalFilename", "signal.exe"
+		VALUE "CompanyName", ""
+		VALUE "LegalCopyright", "\xA9 Developed by Fedor Ivanov"
+		VALUE "ProductName", "SIGNAL"
+		VALUE "FileVersion", "v0.18"
+		VALUE "ProductVersion", "v0.18"
+	}
+}
+
+BLOCK "VarFileInfo"
+{
+	VALUE "Translation", 0x0409 0x04B0  
+}
+}   
+"""
+
+chdir(WORKDIR)
+
+with open(OUTPUT_FILE, 'w') as output_file:
+    output_file.write(VERSION_INFO)
