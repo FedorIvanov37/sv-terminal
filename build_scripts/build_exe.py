f69f396871804252e444da19342380d9de43e7da
admin <fedorivanov37@gmail.com>
2024-05-19 11:18:42 +0300

    Hardcoded version info

--- build_scripts/build_exe.py
+++ build_scripts/build_exe.py
@@ -9,7 +9,7 @@ PyInstaller.__main__.run(
         "--clean",
         "--onefile",
         "--hide-console=hide-early",
-        "--icon=common/data/style/logo_triangle.ico",
+        "--icon=NONE", # common/data/style/logo_triangle.ico",
         f"--distpath={getcwd()}",
         "--log-level=INFO",
     ]
