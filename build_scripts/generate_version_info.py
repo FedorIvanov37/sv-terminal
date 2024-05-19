893e4f76ef243d46dff28504b52eed4ee25dc262
admin <fedorivanov37@gmail.com>
2024-05-19 05:47:41 +0300

    Add dynamic version info

--- build_scripts/generate_version_info.py
+++ build_scripts/generate_version_info.py
@@ -1,35 +1,36 @@
 from os import chdir
+from common.lib.enums.ReleaseDefinition import ReleaseDefinition
 
 
 WORKDIR = "build"
 OUTPUT_FILE = "version_info.rc"
-VERSION_INFO = r"""
+VERSION_INFO = f"""
 1 VERSIONINFO
-FILEVERSION 0,0,0,18
-PRODUCTVERSION 0,0,0,18
+FILEVERSION 0,0,0,{ReleaseDefinition.VERSION_NUMBER}
+PRODUCTVERSION 0,0,0,{ReleaseDefinition.VERSION_NUMBER}
 FILEOS 0x40004
 FILETYPE 0x1
-{
+{{
 BLOCK "StringFileInfo"
-{
+{{
 	BLOCK "040904B0"
-	{
-		VALUE "FileDescription", "Simplified ISO generation algorithm"
-		VALUE "InternalName", "signal.exe"
-		VALUE "OriginalFilename", "signal.exe"
+	{{
+		VALUE "FileDescription", "Signal executable file"
+		VALUE "InternalName", "{ReleaseDefinition.NAME}.exe"
+		VALUE "OriginalFilename", "{ReleaseDefinition.NAME}.exe"
 		VALUE "CompanyName", ""
-		VALUE "LegalCopyright", "\xA9 Developed by Fedor Ivanov"
-		VALUE "ProductName", "SIGNAL"
-		VALUE "FileVersion", "v0.18"
-		VALUE "ProductVersion", "v0.18"
-	}
-}
+		VALUE "LegalCopyright", "{r'\xA9'} Developed by Fedor Ivanov"
+		VALUE "ProductName", "{ReleaseDefinition.NAME.upper()}"
+		VALUE "FileVersion", "{ReleaseDefinition.VERSION}"
+		VALUE "ProductVersion", "{ReleaseDefinition.VERSION}"
+	}}
+}}
 
 BLOCK "VarFileInfo"
-{
+{{
 	VALUE "Translation", 0x0409 0x04B0  
-}
-}   
+}}
+}}   
 """
 
 chdir(WORKDIR)
